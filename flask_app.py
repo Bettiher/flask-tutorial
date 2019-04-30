be42c7ed10f5b1be3c07dbd56f62a70bbe00a7bb
bettiher <bettina.hermant@bbc.co.uk>
2019-04-30 11:43:14 +0100

    return list of json

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -9,6 +9,12 @@ def hello():
     return "Hello!!"
 
 
-@app.route("/bla/")
+@app.route("/bla")
 def blabla():
     return "Blabla!"
+
+
+@app.route("/api")
+def data_jsonify():
+    data = [{"col 1": "a", "col 2": "b"}, {"col 1": "c", "col 2": "d"}]
+    return jsonify(data)
